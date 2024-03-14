bdcea5af657964e61644a43b827d359a793679a7
Jan Kubowicz <j.kubowicz7@gmail.com>
2024-03-14 16:08:58 +0100

    added appbar both for login and register page

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -23,143 +23,143 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromRGBO(128, 128, 0, 1)
+      ),
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        top: true,
-        child: Container(
-          decoration: const BoxDecoration(color: Color.fromRGBO(128, 128, 0, 1)),
-          child: Column(
-            children: <Widget>[
-              const Align(
-                alignment: Alignment.topCenter,
-                child: Image(
-                  image: AssetImage('lib/images/register_icon.png'),
-                  height: 100,
-                  width: 100
-                )
-              ),
-
-              Expanded(
-                child: Column(
-                  children: [
-                    const SizedBox(height: 60),
-
-                    const Center(
-                      child: Text('Welcome! Try to Sign Up',
-                        style: TextStyle(fontSize: 16)),
-                    ),
-
-                    const SizedBox(height: 60),
-                    
-                    LoginTextField(
-                      controller: emailController,
-                      inLabelText: "Email",
-                      inHintText: "name@example.com"
-                    ),
-                    
-                    const SizedBox(height: 20),
-        
-                    PasswordTextField(
-                      controller: passwordController,
-                      inLabelText: "Password",
-                      inHintText: "password",
-                    ),
-                    
-                    const SizedBox(height: 20),
-        
-                    PasswordTextField(
-                      controller: confirmPasswordController,
-                      inLabelText: "Confirm Password",
-                      inHintText: "confirm password",
-                    ),
-                    
-                    const SizedBox(height: 30),
-        
-                    LRButton(inText: "Sign Up", onPressed: () => {
-                      authenticationServices.signUpWithEmail(
-                        emailController.text,
-                        passwordController.text,
-                        confirmPasswordController.text
-                      )
-                    }),
-
-                    const SizedBox(height: 40),
-
-                    const Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 15.0),
-                            child: Divider(thickness: 2, color: Colors.black)
-                          ),
-                        ),       
-
-                        Center(
-                          child: Text("or continue with",
-                          style: TextStyle(fontSize: 16),)
+      body: Container(
+        decoration: const BoxDecoration(color: Color.fromRGBO(128, 128, 0, 1)),
+        child: Column(
+          children: <Widget>[
+            const Align(
+              alignment: Alignment.topCenter,
+              child: Image(
+                image: AssetImage('lib/images/register_icon.png'),
+                height: 100,
+                width: 100
+              )
+            ),
+      
+            Expanded(
+              child: Column(
+                children: [
+                  const SizedBox(height: 60),
+      
+                  const Center(
+                    child: Text('Welcome! Try to Sign Up',
+                      style: TextStyle(fontSize: 16)),
+                  ),
+      
+                  const SizedBox(height: 30),
+                  
+                  LoginTextField(
+                    controller: emailController,
+                    inLabelText: "Email",
+                    inHintText: "name@example.com"
+                  ),
+                  
+                  const SizedBox(height: 20),
+      
+                  PasswordTextField(
+                    controller: passwordController,
+                    inLabelText: "Password",
+                    inHintText: "password",
+                  ),
+                  
+                  const SizedBox(height: 20),
+      
+                  PasswordTextField(
+                    controller: confirmPasswordController,
+                    inLabelText: "Confirm Password",
+                    inHintText: "confirm password",
+                  ),
+                  
+                  const SizedBox(height: 30),
+      
+                  LRButton(inText: "Sign Up", onPressed: () => {
+                    authenticationServices.signUpWithEmail(
+                      emailController.text,
+                      passwordController.text,
+                      confirmPasswordController.text
+                    )
+                  }),
+      
+                  const SizedBox(height: 40),
+      
+                  const Row(
+                    children: [
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15.0),
+                          child: Divider(thickness: 2, color: Colors.black)
                         ),
-
-                        Expanded(
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 15.0),
-                            child: Divider(thickness: 2, color: Colors.black)
-                          ),
-                        ), 
-                      ],
-                    ),
-
-                    const SizedBox(height: 30),
-
-                    Center(
-                      child: GestureDetector(
-                        onTap: () => authenticationServices.signInWithGoogle(),
-                        child: Container(
-                          padding: const EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            border: Border.all(color: Colors.black),
-                            borderRadius: BorderRadius.circular(10),
-                            color: const Color.fromRGBO(128, 128, 0, 0)
-                          ),
-                          child: const Image(
-                            image: AssetImage('lib/images/google_icon.png'), 
-                            height: 50,
-                          )
+                      ),       
+      
+                      Center(
+                        child: Text("or continue with",
+                        style: TextStyle(fontSize: 16),)
+                      ),
+      
+                      Expanded(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15.0),
+                          child: Divider(thickness: 2, color: Colors.black)
                         ),
+                      ), 
+                    ],
+                  ),
+      
+                  const SizedBox(height: 30),
+      
+                  Center(
+                    child: GestureDetector(
+                      onTap: () => authenticationServices.signInWithGoogle(),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius: BorderRadius.circular(10),
+                          color: const Color.fromRGBO(128, 128, 0, 0)
+                        ),
+                        child: const Image(
+                          image: AssetImage('lib/images/google_icon.png'), 
+                          height: 50,
+                        )
                       ),
                     ),
-                  ]
-                )
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text("Already have an account? ",
-                    style: TextStyle(fontSize: 16)
                   ),
-
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: GestureDetector(
-                      onTap: () {
-                        MyApp.navigatorKey.currentState!.pushReplacement(
-                          MaterialPageRoute(builder: (context) => const LoginPage()),
-                        );
-                      },
-                      child: const Text("Sign In",
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16
-                        )
+                ]
+              )
+            ),
+      
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text("Already have an account? ",
+                  style: TextStyle(fontSize: 16)
+                ),
+      
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: GestureDetector(
+                    onTap: () {
+                      MyApp.navigatorKey.currentState!.pushReplacement(
+                        MaterialPageRoute(builder: (context) => const LoginPage()),
+                      );
+                    },
+                    child: const Text("Sign In",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16
                       )
                     )
                   )
-                ]
-              )
-              
-              
-            ],
-          ),
+                )
+              ]
+            )
+            
+            
+          ],
         ),
       ),
     );
