fbbfd6d43169dfcfd7a4b8cd09a106e6e14a16ba
Jan Kubowicz <j.kubowicz7@gmail.com>
2024-03-16 20:05:13 +0100

    wip drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:camera/camera.dart';
 import 'package:cragon/main.dart';
 import 'package:cragon/pages/camera_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cragon/services/authentication_services.dart';
 
@@ -39,11 +40,12 @@ class _HomeState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(128, 128, 0, 1),
-        title: const Text("Home Page"),
+        iconTheme: const IconThemeData(color: Color.fromRGBO(128, 128, 0, 1)),
+        backgroundColor: const Color.fromRGBO(38, 45, 53, 1),
+        title: const Text("Home Page", style: TextStyle(color: Color.fromRGBO(128, 128, 0, 1)),),
         actions: <Widget> [
           IconButton(
-            icon: const Icon(Icons.camera, color: Colors.black,),
+            icon: const Icon(Icons.camera),
             tooltip: 'Open Camera',
             onPressed: () {
               MyApp.navigatorKey.currentState!.push(
@@ -53,21 +55,42 @@ class _HomeState extends State<HomePage> {
         ],
       ),
       drawer: Drawer(
+        backgroundColor: const Color.fromRGBO(38, 45, 53, 1),
         child: ListView(
           padding: EdgeInsets.zero,
           children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(128, 128, 0, 1),
+            DrawerHeader(
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    backgroundColor: Colors.grey[300],
+                    maxRadius: 45,
+                    child: Text(
+                      FirebaseAuth.instance.currentUser!.email.toString()[0].toUpperCase(),
+                      style: const TextStyle(fontSize: 25)
+                    ), //first letter of username
+                  ),
+                  Text(FirebaseAuth.instance.currentUser!.email.toString(),
+                    style: const TextStyle(color: Color.fromRGBO(128, 128, 0, 1))
+                  ),
+                ],
               ),
-              child: Text('Drawer Header'),
             ),
 
             ListTile(
-              tileColor: const Color.fromRGBO(128, 128, 0, 1),
-              leading: const Icon(Icons.logout,color: Colors.white,),
-              title: const Text("Log out"),
-              titleTextStyle: const TextStyle(color: Colors.white),
+              leading: const Icon(Icons.settings, color: Color.fromRGBO(128, 128, 0, 1),),
+              title: const Text("Settings", style: TextStyle(fontSize: 18)),
+              titleTextStyle: const TextStyle(color: Color.fromRGBO(128, 128, 0, 1)),
+              onTap: () {
+                MyApp.navigatorKey.currentState!.push(
+                  MaterialPageRoute(builder: (context) => Placeholder()));
+              },
+            ),
+
+            ListTile(
+              leading: const Icon(Icons.logout, color: Color.fromRGBO(128, 128, 0, 1),),
+              title: const Text("Log out", style: TextStyle(fontSize: 18)),
+              titleTextStyle: const TextStyle(color: Color.fromRGBO(128, 128, 0, 1)),
               onTap: () {
                 authenticationServices.signOutCurrentUser();
               },
