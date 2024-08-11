829c4238648ff8a2c50d661e7ca79b59003cc755
Jan Kubowicz <j.kubowicz7@gmail.com>
2024-08-11 21:38:01 +0200

    add single scroll view to login and register page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -20,11 +20,11 @@ class _LoginPageState extends State<LoginPage> {
   final AuthenticationServices authenticationServices = AuthenticationServices();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-  
+
   @override
   void initState() {
     super.initState();
-    
+
     developer.log(
       name: "LoginPage -> initState",
       "Current user has entered LoginPage");
@@ -34,152 +34,156 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(128, 128, 0, 1),
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(128, 128, 0, 1)
-      ),
       resizeToAvoidBottomInset: false,
-      body: Column(
-        children: <Widget>[
-          const Align(
-            alignment: Alignment.topCenter,
-            child: Image(
-              image: AssetImage('lib/images/lock_icon.png'),
-              height: 100,
-              width: 100
-            )
-          ),
-            
-          Expanded(
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
-            
-                const Center(
-                  child: Text('Welcome! Try to Sign In',
-                    style: TextStyle(fontSize: 16)),
-                ),
-            
-                const SizedBox(height: 50),
-            
-                FormTextItem(
-                  controller: emailController,
-                  inLabelText: "Email",
-                  inHintText: "name@example.com",
-                  prefixIcon: const Icon(Icons.email, color: Colors.black),
-                  isPasswordForm: false
-                ),
-            
-                const SizedBox(height: 30),
-            
-                FormTextItem(
-                  controller: passwordController,
-                  inLabelText: "Password",
-                  inHintText: 'Your Password',
-                  prefixIcon: const Icon(Icons.lock, color: Colors.black),
-                  isPasswordForm: true
-                ),
-            
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          MyApp.navigatorKey.currentState!.push(
-                            MaterialPageRoute(builder: (context) => const ForgotPasswordPage()),
-                          );
-                        },
-                        child: const Text('Forgot password?',
-                          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)
-                        ),
-                      )
-                    ]
-                  )
-                ),
-            
-                const SizedBox(height: 50),
-            
-                LRButton(inText: "Sign In", onPressed: () => {
-                  authenticationServices.signInWithEmail(
-                    emailController.text, passwordController.text)
-                }),
-            
-                const SizedBox(height: 60),
-            
-                const Row(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              const Align(
+                alignment: Alignment.topCenter,
+                child: Image(
+                  image: AssetImage('lib/images/lock_icon.png'),
+                  height: 100,
+                  width: 100
+                )
+              ),
+                
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Column(
                   children: [
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15.0),
-                        child: Divider(thickness: 2, color: Colors.black)
-                      ),
+                    const SizedBox(height: 50),
+                
+                    const Center(
+                      child: Text('Welcome! Try to Sign In',
+                        style: TextStyle(fontSize: 16)),
                     ),
-            
-                    Center(
-                      child: Text("or continue with",
-                        style: TextStyle(fontSize: 16),
+                
+                    const SizedBox(height: 50),
+                
+                    FormTextItem(
+                      controller: emailController,
+                      inLabelText: "Email",
+                      inHintText: "name@example.com",
+                      prefixIcon: const Icon(Icons.email, color: Colors.black),
+                      isPasswordForm: false
+                    ),
+                
+                    const SizedBox(height: 30),
+                
+                    FormTextItem(
+                      controller: passwordController,
+                      inLabelText: "Password",
+                      inHintText: 'Your Password',
+                      prefixIcon: const Icon(Icons.lock, color: Colors.black),
+                      isPasswordForm: true
+                    ),
+                
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          GestureDetector(
+                            onTap: () {
+                              MyApp.navigatorKey.currentState!.push(
+                                MaterialPageRoute(builder: (context) => const ForgotPasswordPage()),
+                              );
+                            },
+                            child: const Text('Forgot password?',
+                              style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)
+                            ),
+                          )
+                        ]
                       )
                     ),
-            
-                    Expanded(
-                      child: Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 15.0),
-                        child: Divider(thickness: 2, color: Colors.black)
+                
+                    const SizedBox(height: 50),
+                
+                    LRButton(inText: "Sign In", onPressed: () => {
+                      authenticationServices.signInWithEmail(
+                        emailController.text, passwordController.text)
+                    }),
+                
+                    const SizedBox(height: 60),
+                
+                    const Row(
+                      children: [
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15.0),
+                            child: Divider(thickness: 2, color: Colors.black)
+                          ),
+                        ),
+                
+                        Center(
+                          child: Text("or continue with",
+                            style: TextStyle(fontSize: 16),
+                          )
+                        ),
+                
+                        Expanded(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15.0),
+                            child: Divider(thickness: 2, color: Colors.black)
+                          ),
+                        ), 
+                      ],
+                    ),
+                
+                    const SizedBox(height: 30),
+                
+                    Center(
+                      child: GestureDetector(
+                        onTap: () => authenticationServices.signInWithGoogle(),
+                        child: Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color.fromRGBO(128, 128, 0, 0)
+                          ),
+                          child: const Image(
+                            image: AssetImage('lib/images/google_icon.png'), 
+                            height: 50,
+                          )
+                        ),
                       ),
-                    ), 
+                    ),
                   ],
                 ),
-            
-                const SizedBox(height: 30),
-            
-                Center(
-                  child: GestureDetector(
-                    onTap: () => authenticationServices.signInWithGoogle(),
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black),
-                        borderRadius: BorderRadius.circular(10),
-                        color: const Color.fromRGBO(128, 128, 0, 0)
-                      ),
-                      child: const Image(
-                        image: AssetImage('lib/images/google_icon.png'), 
-                        height: 50,
-                      )
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-            
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text("Don't have an account? ",
-                style: TextStyle(fontSize: 16)
               ),
-            
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: GestureDetector(
-                  onTap: () {
-                    MyApp.navigatorKey.currentState!.pushReplacement(
-                      MaterialPageRoute(builder: (context) => const RegisterPage()),
-                    );
-                  },
-                  child: const Text("Sign Up!",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16
+                
+              const SizedBox(height: 30),
+                
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account? ",
+                    style: TextStyle(fontSize: 16)
+                  ),
+                
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: GestureDetector(
+                      onTap: () {
+                        MyApp.navigatorKey.currentState!.pushReplacement(
+                          MaterialPageRoute(builder: (context) => const RegisterPage()),
+                        );
+                      },
+                      child: const Text("Sign Up!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16
+                        )
+                      )
                     )
                   )
-                )
+                ]
               )
-            ]
-          )
-        ],
+            ],
+          ),
+        ),
       ),
     );
   }
